c011f03fadaffd6c4484f76754b3593373fd1c04
AshmweGeoffrey <ashimwegeoffrey@gmail.com>
2024-10-30 21:36:00 +0200

    Pure React Components fix

--- 0x00-intro_react/src/app.js
+++ 0x00-intro_react/src/app.js
@@ -1,21 +1,36 @@
-const pet =() => {
+const pet =(props) => {
 	return React.createElement("div",{},[
-		React.createElement("h1",{},"Luna"),
-		React.createElement("h2",{},"Dog"),
-		React.createElement("h2",{},"Havanesse"),
+		React.createElement("h1",{},props.name),
+		React.createElement("h2",{},props.animal),
+		React.createElement("h2",{},props.breed),
 	]);
 };
 const App=() => {
 		  return React.createElement("div",{},
 			  [
 			  React.createElement("h1",{},"Adopt Me"),
-			  React.createElement(pet),
-			  React.createElement(pet),
-			  React.createElement(pet),
+			  React.createElement(pet,
+				  {
+					  animal:"dog",
+					  name:"Luna",
+					  breed:"havanesse"
+				  }),
+			  React.createElement(pet,
+				  {
+					  animal:"cat",
+					  name:"Sam",
+					  breed:"Bat-cat"
+				  }),
+			  React.createElement(pet,
+				  {
+					  animal:"Hippo",
+					  name:"Ben",
+					  breed:"MAMUS-HIPPO"
+				  }),
 			 
 ]
-		  )
-	  };
-	  const container = document.getElementById('root');
-	  const root =ReactDOM.createRoot(container);
-	  root.render(React.createElement(App))
+		  );
+};
+const container = document.getElementById('root');
+const root =ReactDOM.createRoot(container);
+root.render(React.createElement(App));
