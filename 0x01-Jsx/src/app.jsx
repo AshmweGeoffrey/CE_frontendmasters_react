3805fdb451ba819617c6404360e030f5983fba9f
AshmweGeoffrey <ashimwegeoffrey@gmail.com>
2024-10-31 23:28:13 +0200

    Fix: Rendering in Jsx(0x01-jsx File:app.jsx)

--- 0x01-Jsx/src/app.jsx
+++ 0x01-Jsx/src/app.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Pet from "./Pet";
 const App = () => {
-  <div>
-    <h1>Adopt Me!</h1>
-    <Pet name="luna" animal="dog" breed="havaneese" />
-    <Pet name="Perpper" animal="Bird" breed="nan-1" />
-    <Pet name="Daink" animal="Cat" breed="nan-2" />
-  </div>;
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="luna" animal="dog" breed="havaneese" />
+      <Pet name="Perpper" animal="Bird" breed="nan-1" />
+      <Pet name="Daink" animal="Cat" breed="nan-2" />
+    </div>
+  );
 };
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
